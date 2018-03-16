e18d51048921f64d94bb1e0f7f487875fed47b50
Ivan Siderov <ivan.siderov@quenti.tech>
2018-03-16 20:31:32 +0200

    added: further card css refactoring

--- frontend/components/Card.jsx
+++ frontend/components/Card.jsx
@@ -11,8 +11,8 @@ export default class Card extends React.Component {
       <div className='card'>
         <img src="https://picsum.photos/300/300/?random" alt="card-image" />
         <div className="card-main-info">
-          <div className="card-price">$420</div>
-          <div className="card-name">Lorem ipsum something name</div>
+          <div className="card-price"><strong>$420</strong></div>
+          <div className="card-name"><strong>Lorem ipsum something name</strong></div>
         </div>
         <div className="card-description">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
         <div className="card-share-options"></div>
