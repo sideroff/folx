5d6eea9ade083933ad696b56a16aa6b577717e8f
sideroff <ivan.sideroff@gmail.com>
2018-03-17 13:04:22 +0200

    refactoring of header, center items

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -10,18 +10,21 @@ export default class Header extends React.Component {
     return (
       <header>
         <div className="header-top-bar"></div>
-        <div className="header-logo">
-          <Link to='/'>
-            <img src="/logo.png" alt="logo" />
-            <img src="mobile-logo.png" alt="mobile.logo" />
-          </Link>
+        <div className="header-main">
+          <div className="header-logo">
+            <Link to='/'>
+              <img src="/logo.png" alt="logo" />
+              <img src="mobile-logo.png" alt="mobile.logo" />
+            </Link>
+          </div>
+          <nav className="header-nav">
+            <Link to='/profile'>Profile</Link>
+            <Link to='/ad-create'>New Ad</Link>
+            <Link to='/ads'>Posts</Link>
+            <Link to='/about'>About</Link>
+          </nav>
         </div>
-        <nav className="header-nav">
-          <Link to='/profile'>Profile</Link>
-          <Link to='/ad-create'>New Ad</Link>
-          <Link to='/ads'>Posts</Link>
-          <Link to='/about'>About</Link>
-        </nav>
+
 
       </header>
     )
