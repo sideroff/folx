65acf997fa2344b90520f4acd8071dcf57b58e61
sideroff <ivan.sideroff@gmail.com>
2018-03-21 08:39:22 +0200

    added: initial auth form

--- frontend/components/Authentication.jsx
+++ frontend/components/Authentication.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 import Form from './Form.jsx'
 import forms from './../forms'
 
-export default class Authentication extends React.Component {
+import actionTypes from './../actionTypes'
+
+function mapStateToProps(state) {
+  return {}
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    toggleAuthenticationForm: (formName) {
+      dispatch({ type: actionTypes.TOGGLE_AUTHENTICATION_FORM, payload: formName })
+    }
+  }
+}
+
+class Authentication extends React.Component {
   constructor(props) {
     super(props)
   }
@@ -11,9 +26,18 @@ export default class Authentication extends React.Component {
   render() {
     return (
       <div>
-        <Form config={forms.login} />
-        <Form config={forms.register} />
+        <div>
+          <Form config={forms.login} />
+          <Form config={forms.register} />
+        </div>
+        <div>
+          <Link to="/login">Login</Link>
+          <Link to="/register">Register</Link>
+        </div>
       </div>
     )
   }
 }
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Authentication)
